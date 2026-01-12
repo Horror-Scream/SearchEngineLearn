e8a1e684c1d02f222a206260d071037df6617490
Horror-Scream <horrorscream6063@gmail.com>
2026-01-13 01:28:46 +0300

    exception

---
+++ src/main/java/searchengine/exceptions/BadRequestException.java
@@ -0,0 +1,13 @@
+package searchengine.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class BadRequestException  extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/searchengine/exceptions/InternalServerErrorException.java
@@ -0,0 +1,17 @@
+package searchengine.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+@Slf4j
+public class InternalServerErrorException extends RuntimeException {
+    public InternalServerErrorException(String message) {
+        super(message);
+    }
+
+    public InternalServerErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
